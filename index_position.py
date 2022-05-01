5480e0fcc6b85af7889e0c137a2ee3fa6c8846f0
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-05-01 17:51:15 +0530

    Add files via upload

---
+++ index_position.py
@@ -0,0 +1,62 @@
+"""
+Date: 25th April 2022 Monday
+Name: Hd
+File Desc: String Slicing
+"""
+
+# x='department'
+# print(x)
+# print(x[0]) # d
+# print(x[1]) # e
+# print(x[2]) # p
+# print(x[3]) # a
+# print(x[9]) # t
+# print(x[10]) # IndexError: string index out of range
+
+# x='department'
+# print(x[2]+x[3]+x[4]+x[5]) # part
+# print(x[7]+x[8]+x[0]) # end
+# print(x[3]+x[2]+x[3]+x[4]+x[5]+x[6]+x[7]+x[8]+x[9])
+# print(x[3]+x[10])
+
+# x='python'
+# print(x[:])  # python
+# print(x[0:]) # python
+# print(x[1:]) # ython
+# print(x[2:]) # thon
+# print(x[3:]) # hon
+# print(x[4:]) # on
+# print(x[5:]) # n
+# print(x[6:]) # blank line or empty line
+# print(x[0:5]) # pytho
+# print(x[2:4]) # th
+# print(x[2:3]) # t
+# print(x[5:1]) # blank line or empty line
+# print(x[4:3]) # blank line or empty line
+# print(x[:6]) # python
+
+# x='python'
+# print(x[-1]) # n
+# print(x[-3]) # h
+# print(x[-7]) # IndexError: string index out of range
+# print(x[:-1]) # pytho
+# print(x[:-3]) # pyt
+# print(x[:-6]) #  blank line or empty line
+# print(x[2:-4]) # blank line or empty line
+# print(x[-5:-1]) # ytho
+
+# x='python'
+# print(x[::]) # python
+# print(x[::1]) # python
+# print(x[0::1]) # python
+# print(x[0::2]) # pto
+# print(x[0::3]) # ph
+# print(x[0::4]) # po
+# print(x[0::5]) # pn
+# print(x[0::6]) # p
+
+# x='python'
+# print(x[::-1]) # nohtyp
+# print(x[::-2]) # nhy
+# print(x[::-3]) # nt
+# print(x[::-4]) # ny
---
+++ intro_end_sep.py
@@ -0,0 +1,54 @@
+# print('hello'+'y') # helloy
+# print('hello'+''+'y') # helloy
+# print('hello'+' '+'y') # hello y
+# print('hello'+'   '+'y') # hello   y
+# print('hello'+'-'+'y') # hello-y
+# print('hello'+'@'+'y') # hello@y
+
+""" sep method """
+# print('h','y') # h y 
+# print('h','y',sep='') # hy 
+# print('h','y',sep=' ') # h y 
+# print('h','y',sep='@') # h@y 
+# print(1,2,3,4,5,6,sep='-') # 1-2-3-4-5-6
+# print('Jerome','Soorya','Python',sep='@')
+
+# print('h' 'y') # hy
+# print('h'            'y') # hy
+# print('h '    'y') # h y
+# print('h'    ' y') # h y
+# print('h','y') # h y
+# print('h'+'    '+'y') # h    y
+# print("python",3,'Version Used is','3.10.4')
+# print("python"+' '+str(3)+' '+'Version Used is'+' '+'3.10.4')
+# print("python",3,'Version Used is','3.10.4',sep='-')
+
+# print('hello')
+# print('bye')
+
+
+# print('hello',end='')
+# print('bye')
+
+# print('hello',end=',')
+# print('bye')
+
+# print('hello',end=' ')
+# print('bye')
+
+# print(1,end=' ')
+# print(2)
+# print(3)
+
+# print(1,end=' ')
+# print(2,end=' ')
+# print(3)
+
+# print(1,end='')
+# print(2,end='')
+# print(3)
+
+# print('c'  'b') # cb
+# print('c'  'b',sep=' ') # cb
+
+# print(1,2,3,4,5,end='.')
